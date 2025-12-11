dac78b20109f9bb92bd021044a84b990a618c890
Zeus0809 <ikkit2002@gmail.com>
2025-12-11 11:39:04 +0100

    Clean Slate: start developing the FastAPI + React app with Gemini API.

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import llm
-
 def main():
 
-    llm.download()
-    llm.init()
-
-    prompt = input('Ask me anything: [q to quit]')
-    while prompt != "q":
-        llm.chat(prompt)
-        prompt = input('Ask me anything: [q to quit]')
+    pass
 
 
 
