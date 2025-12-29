c94bc70a2efde3795dd8fb4952c07b7afe56c984
Zeus0809 <ikkit2002@gmail.com>
2025-12-29 18:40:56 +0100

    Implemented Gemini usage logging and reading the logs from GCP. Gonna test if json deserialization works or not.

--- backend/utils.py
+++ backend/utils.py
@@ -1,4 +1,9 @@
-import io, wave
+import io, wave, os, json
+from dotenv import load_dotenv
+from typing import List, Dict
+from google.cloud import logging_v2
+from google.cloud.logging_v2 import DESCENDING
+from fastapi import HTTPException
 
 def convert_audio_to_wav(pcm_data: bytes) -> bytes:
         """Convert raw PCM data from Gemini TTS into a WAV audio blob"""
@@ -10,3 +15,25 @@ def convert_audio_to_wav(pcm_data: bytes) -> bytes:
             wav_file.writeframes(pcm_data)
         return wav_buffer.getvalue()
 
+def read_gemini_logs() -> List[Dict]:
+    """
+    Access GCP Cloud Run logs for this app and retrieve the latest Gemini usage stats.
+    We need this to pick up the logging from where it left off during the last application run.
+    """
+    load_dotenv()
+    client = logging_v2.Client(project=os.getenv('GCP_PROJECT_ID')) # init the logging client
+    filter_str = 'textPayload:"CUSTOM_TEST"'
+    entries = client.list_entries( # this returns a generator
+         filter_=filter_str,
+         order_by=DESCENDING
+    )
+    # retrieve the latest log entry from generator
+    for entry in entries:
+        latest_entry = entry.payload
+        break
+    try: # deserialize
+        latest_entry = json.loads(latest_entry.replace('GEMINI: ', ''))
+    except json.JSONDecodeError as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="Failed to parse Gemini API logs from GCP")
+    return latest_entry
